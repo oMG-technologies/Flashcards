bfb2aa4948263552072956924dd302c83958ce2a
MarcinMG-web <gierada.marcin@gmail.com>
2021-04-12 15:39:42 +0200

    Fix display on mobile

--- src/Components/FlashCard.js
+++ src/Components/FlashCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FlashCard = () => {
   return (
     <div className='mainContainer'>
-      <p className='mainContainer_text'>Mouse to the card to check answer:</p>
+      <div className='mainContainer_text'>Mouse to the card to check answer:</div>
       <div className='theCard'>
         <div className='theFront'>Front</div>
         <div className='theBack'>Back</div>

9e38a2ac7c077925bea4fe174338a453ced19208
MarcinMG-web <gierada.marcin@gmail.com>
2021-04-26 12:08:07 +0200

    Create context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
 import FlashCard from './components/FlashCard';
 
+import {MainContext} from './context/MainContext';
+
+import {getAllQuestions, getQuestionById} from './services/ApiService';
+
 function App() {
+
+  const [questions, setQuestion] = useState([])
+
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+  const getQuestions = async() => {
+    const dataQuestions = await getAllQuestions()
+    setQuestion(dataQuestions)
+  }
+  // console.log(questions)
+
   return (
-    <div className='App'>
-      <FlashCard />
-    </div>
+    <MainContext.Provider value={questions}>
+      <div className='App'>
+        <FlashCard />
+      </div>
+    </MainContext.Provider> 
   );
 }
 
--- src/components/FlashCard.js
+++ src/components/FlashCard.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import TheFront from './TheFront';
 import TheBack from './TheBack';
 
+import { MainContext } from '../context/MainContext';
+
 const FlashCard = () => {
+
+  const questions = useContext(MainContext)
+  console.log(questions)
+
   return (
     <div className='mainContainer'>
-        <div className='mainContainer_text'>
-          Mouse to the card to check answer:
-        </div>
+      <div className='mainContainer_text'>
+        Mouse to the card to check answer:
+      </div>
       <div className='mainContainer_result'>Result: </div>
-        <div className='theCard'>
-          <TheFront />
-          <TheBack />
-        </div>
+      <div className='theCard'>
+        <TheFront />
+        <TheBack />
+      </div>
       <div className='mainContainer_button-next'>
-        
         <button className='btn_Next'>Next </button>
-
       </div>
     </div>
   );

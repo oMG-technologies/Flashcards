e0f825640aa1ee5abdf9cd1c3f72c4fe8883af19
MarcinMG-web <gierada.marcin@gmail.com>
2021-05-17 09:02:52 +0200

    Add style to the dash board

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,49 +1,40 @@
-import React,{useContext} from 'react'
-import { MainContext } from '../context/MainContext';
+import React from 'react';
 
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
+const Dashboard = ({ numerElemetInFlashCard, setNumerElemetInFlashCard }) => {
+  return (
+    <div className='ContainerDashboard'>
+      <div className='ContainerDashboard_header'>Settings:</div>
 
-
-const Dashboard = ({numerElemetInFlashCard,setNumerElemetInFlashCard}) => {
-
-    // const { setNumerElemetInFlashCard } = useContext(MainContext);
-
-
-
-    
-
-    return (
-        <div className = 'ContainerDashboard'>
-            <div className = 'ContainerDashboard_header'>
-                DashBoard
-            </div>
-
-            <div className = 'ContainerDashboard_main'>
-                How many flash card use? <br />
-                <label>Choise number of flash card</label> <br />
-
-                <input
-                    type='range'
-                    min='1'
-                    max='20'
-                    value={numerElemetInFlashCard}
-                    onChange={(e) => setNumerElemetInFlashCard(e.target.value)} 
-                />
-                <div>{numerElemetInFlashCard}</div>
-
-            </div>
-
-            <div className = 'ContainerDashboard_button'>
-                {/* <button className = 'btn_toTheFlashCard' onClick={nextStep}>Start</button> */}
-                <Link  to = './FlashCard' className = 'btn_toTheFlashCard'>Start</Link>
-            </div>
-            
-           
-    
-          
+      <div className='ContainerDashboard_main'>
+        <div
+          className='ContainerDashboard_main-rangeSlider'
+          data-min='1'
+          data-max='20'
+        >
+          <label style={{ margin: '15px' }}>Choose size of flip card:</label>
+          <input
+            type='range'
+            min='1'
+            max='20'
+            value={numerElemetInFlashCard}
+            onChange={(e) => setNumerElemetInFlashCard(e.target.value)}
+            style={{ marginTop: '10px' }}
+          />
+          <div className='ContainerDashboard_main-rangeSlider-result'>
+            {numerElemetInFlashCard}
+          </div>
         </div>
-    )
-}
+      </div>
 
-export default Dashboard
+      <div className='ContainerDashboard_button'>
+        <Link to='./FlashCard' className='btn_toTheFlashCard'>
+          Start
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;

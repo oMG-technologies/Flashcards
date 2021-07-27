2e11a34d9459b2fa80ad0e5330474bdd9e2dc39c
MarcinMG-web <gierada.marcin@gmail.com>
2021-07-27 17:04:55 +0200

    Remove style in login panel and add style in registration panel

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -27,45 +27,56 @@ const Registration = () => {
   };
   return (
     <div className='ContainerRegistration'>
-      <div className='ContainerRegistration_header'>Registration:</div>
+      <div className='ContainerRegistration_header'>Registration Panel:</div>
 
       <div className='ContainerRegistration_main'>
         <div className='ContainerRegistration_main-form'>
           <form onSubmit={isRegistration} ref={formRef}>
-            <label style={{ margin: '15px' }}>Enter your email</label>
-            <input
-              type='email'
-              placeholder='user@domain.com'
-              name='email'
-              onChange={handleChange}
-              style={{ marginTop: '10px' }}
-            />
-            <label style={{ margin: '15px' }}>Enter your user name</label>
-            <input
-              type='text'
-              placeholder='Username'
-              name='userName'
-              onChange={handleChange}
-              style={{ marginTop: '10px' }}
-            />
-            <label style={{ margin: '15px' }}>Enter your password:</label>
-            <input
-              type='password'
-              placeholder='Password'
-              name='password'
-              onChange={handleChange}
-              style={{ marginTop: '10px' }}
-            />
-            <div className='ContainerRegistration_button'>
-              <button type='submit'>Created account</button>
+            <div className='ContainerLogin_main-form-input'>
+              <label style={{ margin: '15px' }}>Enter your email</label>
+              <input
+                type='email'
+                placeholder='user@domain.com'
+                name='email'
+                onChange={handleChange}
+                className='inputLogin'
+              />
+            </div>
 
-              <Link to='./' type='submit' className='btn_toTheFlashCard'>
-                Back to the login panel
-              </Link>
+            <div className='ContainerRegistration_main-form-input'>
+              <label style={{ margin: '15px' }}>Enter your user name</label>
+              <input
+                type='text'
+                placeholder='Username'
+                name='userName'
+                onChange={handleChange}
+                className='inputLogin'
+              />
+            </div>
+
+            <div className='ContainerRegistration_main-form-input'>
+              <label style={{ margin: '15px' }}>Set your password:</label>
+              <input
+                type='password'
+                placeholder='Password'
+                name='password'
+                onChange={handleChange}
+                className='inputLogin'
+              />
+              <div className='ContainerRegistration_main-form-button'>
+                <button type='submit' className='btn_createAccount'>
+                  Created account
+                </button>
+
+                <Link to='./' type='submit' className='btn_start'>
+                  Back to the login panel
+                </Link>
+              </div>
             </div>
           </form>
         </div>
       </div>
+      <div className='ContainerRegistration_footer'></div>
     </div>
   );
 };

264d86e7adcebe597f921bb10d98efda864a3c8b
Maciej Gierada <maciek.gierada@gmail.com>
2021-07-29 12:34:13 +0200

    feat: add registration with correct put request

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,11 +1,14 @@
 import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import { register } from '../../src/services/Register';
 
 const Registration = () => {
   const [registrationParams, setRegistrationParams] = useState({
     username: '',
     email: '',
     password: '',
+    first_name: '',
+    last_name: '',
   });
 
   const formRef = useRef();
@@ -19,7 +22,15 @@ const Registration = () => {
 
   const isRegistration = (e) => {
     e.preventDefault();
-    console.log(registrationParams);
+    const validated_data = {
+      username: registrationParams.username,
+      password: registrationParams.password,
+      email: registrationParams.email,
+      first_name: registrationParams.first_name,
+      last_name: registrationParams.last_name,
+    };
+    console.log(validated_data);
+    register(validated_data);
 
     if (formRef.current) {
       formRef.current.reset();
@@ -32,6 +43,28 @@ const Registration = () => {
       <div className='ContainerRegistration_main'>
         <div className='ContainerRegistration_main-form'>
           <form onSubmit={isRegistration} ref={formRef}>
+            <div className='ContainerLogin_main-form-input'>
+              <label style={{ margin: '15px' }}>Enter your first name:</label>
+              <input
+                type='first_name'
+                placeholder='Zbigniew'
+                name='first_name'
+                onChange={handleChange}
+                className='inputLogin'
+              />
+            </div>
+
+            <div className='ContainerLogin_main-form-input'>
+              <label style={{ margin: '15px' }}>Enter your last name:</label>
+              <input
+                type='last_name'
+                placeholder='Stonoga'
+                name='last_name'
+                onChange={handleChange}
+                className='inputLogin'
+              />
+            </div>
+
             <div className='ContainerLogin_main-form-input'>
               <label style={{ margin: '15px' }}>Enter your email:</label>
               <input
@@ -48,7 +81,7 @@ const Registration = () => {
               <input
                 type='text'
                 placeholder='username'
-                name='userName'
+                name='username'
                 onChange={handleChange}
                 className='inputLogin'
               />
@@ -63,11 +96,12 @@ const Registration = () => {
                 onChange={handleChange}
                 className='inputLogin'
               />
+
               <input className='special' name='field_name' type='text' />
 
               <div className='ContainerRegistration_main-form-button'>
                 <button type='submit' className='btn_createAccount'>
-                  Created account
+                  Create account
                 </button>
 
                 <Link to='./' type='submit' className='btn_start'>

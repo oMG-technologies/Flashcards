07d73d4b88da1adb23559b48892e4b03517c75d2
Maciej Gierada <maciek.gierada@gmail.com>
2021-07-30 10:56:59 +0200

    feat: modify getToken method to use a baseURL

--- src/services/Authorization.js
+++ src/services/Authorization.js
@@ -1,19 +1,26 @@
 import axios from 'axios';
 
-const url = `https://words-translation.herokuapp.com/api-token-auth/`;
+const api = axios.create({
+  baseURL: `https://words-translation.herokuapp.com/`,
+});
+
+// const url_auth = `https://words-translation.herokuapp.com/api-token-auth/`;
 // const url1 = `https://words-translation.herokuapp.com/user_progress/`;
 
-export const getToken = (data) =>
-  axios
-    .post(url, data)
-    .then((res) => {
+export const getToken = async (data) => {
+  try {
+    return await api.post(`/api-token-auth/`, data).then((res) => {
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('user', data.username);
-    })
-    .catch((error) => {
-      console.error(error);
     });
+  } catch (err) {
+    console.error(err);
+  }
+};
 
+const url_is_user = `https://words-translation.herokuapp.com/is_user/`;
+
+// export const isUser = (username) => axios.get(`${username}`);
 // export const getAccessToApplication = (access_token) =>
 //   axios
 //     .get(url1, {

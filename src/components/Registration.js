c1fe126ff11ec91f42abd2203cc9bfee952114bb
MarcinMG-web <gierada.marcin@gmail.com>
2021-08-03 19:31:47 +0200

    Added info for user to verfy your email

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { register } from '../../src/services/Register';
 
@@ -22,7 +22,17 @@ const Registration = () => {
 
   const [isRegistration, setIsRegistration] = useState(false);
 
+  // iRegistration info
+  const [sendFormText, setSendFormText] = useState('');
+  const [showRegistrationInfo, setShowRegistrationInfo] = useState(false);
+
   const formRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      <Redirect from='/Registration' to='/' />;
+    }, 100);
+  }, [isRegistration]);
 
   const handleChange = (e) => {
     setRegistrationParams({
@@ -98,6 +108,10 @@ const Registration = () => {
        * Registration
        */
       register(registrationParams);
+      setShowRegistrationInfo(true);
+      setSendFormText(
+        'Thank you for registering in FlipCards and choosing our product. Your account was successfully created and is almost ready to use. Open your email, click the link below to verify your email so we make sure everything is up and running!'
+      );
       setIsRegistration(true);
     }
 
@@ -106,109 +120,114 @@ const Registration = () => {
     }
   };
 
-  if (isRegistration) {
-    return <Redirect from='/Registration' to='/' />;
-  }
-
   return (
     <div className='ContainerRegistration'>
       <div className='ContainerRegistration_header'>Registration Panel:</div>
 
       <div className='ContainerRegistration_main'>
         <div className='ContainerRegistration_main-form'>
-          <form onSubmit={isRegistrationToApplication} ref={formRef}>
-            <div className='ContainerLogin_main-form-input'>
-              <label style={{ margin: '15px' }}>Enter your first name:</label>
-              <input
-                type='text'
-                placeholder='Zbigniew'
-                name='first_name'
-                onChange={handleChange}
-                onBlur={check}
-                className='inputLogin'
-              />
+          {showRegistrationInfo ? (
+            <div>
+              {sendFormText}
+              <Link to='./' type='submit' className='btn_start'>
+                After that go to login panel
+              </Link>
             </div>
-
-            <span style={{ color: 'red', fontSize: '14px' }}>
-              {errorsValidation.errorFirst_name}
-            </span>
-
-            <div className='ContainerLogin_main-form-input'>
-              <label style={{ margin: '15px' }}>Enter your last name:</label>
-              <input
-                type='text'
-                placeholder='Stonoga'
-                name='last_name'
-                onChange={handleChange}
-                onBlur={check}
-                className='inputLogin'
-              />
-            </div>
-
-            <span style={{ color: 'red', fontSize: '14px' }}>
-              {errorsValidation.errorLast_name}
-            </span>
-
-            <div className='ContainerLogin_main-form-input'>
-              <label style={{ margin: '15px' }}>Enter your email:</label>
-              <input
-                type='email'
-                placeholder='user@domain.com'
-                name='email'
-                onChange={handleChange}
-                onBlur={check}
-                className='inputLogin'
-              />
-            </div>
-
-            <span style={{ color: 'red', fontSize: '14px' }}>
-              {errorsValidation.errorEmail}
-            </span>
-
-            <div className='ContainerRegistration_main-form-input'>
-              <label style={{ margin: '15px' }}>Enter your user name:</label>
-              <input
-                type='text'
-                placeholder='username'
-                name='username'
-                onChange={handleChange}
-                onBlur={check}
-                className='inputLogin'
-              />
-            </div>
-
-            <span style={{ color: 'red', fontSize: '14px' }}>
-              {errorsValidation.errorUsername}
-            </span>
-
-            <div className='ContainerRegistration_main-form-input'>
-              <label style={{ margin: '15px' }}>Set your password:</label>
-              <input
-                type='password'
-                placeholder='password'
-                name='password'
-                onChange={handleChange}
-                onBlur={check}
-                className='inputLogin'
-              />
+          ) : (
+            <form onSubmit={isRegistrationToApplication} ref={formRef}>
+              <div className='ContainerLogin_main-form-input'>
+                <label style={{ margin: '15px' }}>Enter your first name:</label>
+                <input
+                  type='text'
+                  placeholder='Zbigniew'
+                  name='first_name'
+                  onChange={handleChange}
+                  onBlur={check}
+                  className='inputLogin'
+                />
+              </div>
 
               <span style={{ color: 'red', fontSize: '14px' }}>
-                {errorsValidation.errorPassword}
+                {errorsValidation.errorFirst_name}
               </span>
 
-              <input className='special' name='field_name' type='text' />
-
-              <div className='ContainerRegistration_main-form-button'>
-                <Link to='./' type='submit' className='btn_start'>
-                  Back to the login panel
-                </Link>
-
-                <button type='submit' className='btn_createAccount'>
-                  Create account
-                </button>
+              <div className='ContainerLogin_main-form-input'>
+                <label style={{ margin: '15px' }}>Enter your last name:</label>
+                <input
+                  type='text'
+                  placeholder='Stonoga'
+                  name='last_name'
+                  onChange={handleChange}
+                  onBlur={check}
+                  className='inputLogin'
+                />
               </div>
-            </div>
-          </form>
+
+              <span style={{ color: 'red', fontSize: '14px' }}>
+                {errorsValidation.errorLast_name}
+              </span>
+
+              <div className='ContainerLogin_main-form-input'>
+                <label style={{ margin: '15px' }}>Enter your email:</label>
+                <input
+                  type='email'
+                  placeholder='user@domain.com'
+                  name='email'
+                  onChange={handleChange}
+                  onBlur={check}
+                  className='inputLogin'
+                />
+              </div>
+
+              <span style={{ color: 'red', fontSize: '14px' }}>
+                {errorsValidation.errorEmail}
+              </span>
+
+              <div className='ContainerRegistration_main-form-input'>
+                <label style={{ margin: '15px' }}>Enter your user name:</label>
+                <input
+                  type='text'
+                  placeholder='username'
+                  name='username'
+                  onChange={handleChange}
+                  onBlur={check}
+                  className='inputLogin'
+                />
+              </div>
+
+              <span style={{ color: 'red', fontSize: '14px' }}>
+                {errorsValidation.errorUsername}
+              </span>
+
+              <div className='ContainerRegistration_main-form-input'>
+                <label style={{ margin: '15px' }}>Set your password:</label>
+                <input
+                  type='password'
+                  placeholder='password'
+                  name='password'
+                  onChange={handleChange}
+                  onBlur={check}
+                  className='inputLogin'
+                />
+
+                <span style={{ color: 'red', fontSize: '14px' }}>
+                  {errorsValidation.errorPassword}
+                </span>
+
+                <input className='special' name='field_name' type='text' />
+
+                <div className='ContainerRegistration_main-form-button'>
+                  <Link to='./' type='submit' className='btn_start'>
+                    Back to the login panel
+                  </Link>
+
+                  <button type='submit' className='btn_createAccount'>
+                    Create account
+                  </button>
+                </div>
+              </div>
+            </form>
+          )}
         </div>
       </div>
       <div className='ContainerRegistration_footer'></div>

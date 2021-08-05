7e7875974d0f76776a5d692a20879dae4c52829e
MarcinMG-web <gierada.marcin@gmail.com>
2021-08-05 09:36:24 +0200

    Remove span with last name validation in registration component

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -110,10 +110,6 @@ const Registration = () => {
           ) : (
             <form onSubmit={isRegistrationToApplication} ref={formRef}>
 
-              <span style={{ color: 'red', fontSize: '14px' }}>
-                {errorsValidation.errorLast_name}
-              </span>
-
               <div className='ContainerLogin_main-form-input'>
                 <label style={{ margin: '15px' }}>Enter your email:</label>
                 <input

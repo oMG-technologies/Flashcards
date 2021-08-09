2431db1878adbb576864e1f8bd185404f82f1d69
MarcinMG-web <gierada.marcin@gmail.com>
2021-08-09 11:12:50 +0200

    Added TS in NotDound component

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NotFound = () => {
+const NotFound: React.FC = () => {
   return (
     <div className='notFound-container'>
       <div className='notFound-container-text'>

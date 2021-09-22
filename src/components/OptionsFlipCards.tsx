732dcc7b7a694387a205a3f4a7e424b514f962de
MarcinMG-web <gierada.marcin@gmail.com>
2021-09-22 14:24:37 +0200

    Created mock options in flip cards

--- src/components/OptionsFlipCards.tsx
+++ src/components/OptionsFlipCards.tsx
@@ -15,22 +15,18 @@ const OptionsFlipCards: React.FC = () => {
             </span>
           </div>
 
-          <div className='ContainerOptionsFlipCards_main-removeUser'></div>
-          <div className='ContainerOptionsFlipCards_main-selectLanguage'>
-            <label style={{ margin: '15px' }}>Choose language</label>
-          </div>
-
-         
-          <div className='ContainerOptionsFlipCards_main-chooseSide'>
-            <label style={{ margin: '25px' }}>Choose side of cards</label>
+          <div className='ContainerOptionsFlipCards_main-selectOptions'>
+            <label style={{ margin: '15px' }}>Select options:</label>
+            <Link to='/Dashboard' className='btn_toOptionsFlipCards'>
+              Learn English
+            </Link>
+            <Link to='/Dashboard' className='btn_toOptionsFlipCards'>
+              Create own FlipCards
+            </Link>
           </div>
         </div>
 
-        <div className='ContainerOptionsFlipCards_button'>
-          <Link to='/Dashboard' className='btn_toTheFlipCard'>
-            Dashboard
-          </Link>
-        </div>
+        <div className='ContainerOptionsFlipCards_button'></div>
       </div>
     </div>
   );

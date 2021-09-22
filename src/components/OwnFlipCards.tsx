848a208a85814cf2beeecbd6f7f9ff65a7ebe500
MarcinMG-web <gierada.marcin@gmail.com>
2021-09-22 15:00:53 +0200

    Added layout of own flipcards

--- src/components/OwnFlipCards.tsx
+++ src/components/OwnFlipCards.tsx
@@ -3,11 +3,20 @@ import { Link } from 'react-router-dom';
 
 const OwnFlipCards: React.FC = () => {
   return (
-    <div>
-      Own FlipCards
-      <Link to='/OptionsFlipCards' className='btn_toDashboard'>
-        Back
-      </Link>
+    <div className='ContainerOwnFlipCards'>
+      <div className='ContainerOwnFlipCards_header'>Own flip card</div>
+
+      <div className='ContainerOwnFlipCards_main'>
+        <div className='ContainerOwnFlipCards_main-selectOptions'>
+          Place for own FlipCards
+        </div>
+      </div>
+
+      <div className='ContainerOwnFlipCards_button'>
+        <Link to='/OptionsFlipCards' className='btn_toSettings'>
+          Back
+        </Link>
+      </div>
     </div>
   );
 };
